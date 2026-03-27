36f5d1896bd4ee1ca071388dbceca9ba55c630be
이찬영 <dltkfksld1@naver.com>
2026-03-27 11:09:25 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/HairSalonProject2/HairSalonProject2Application.java
@@ -0,0 +1,13 @@
+package com.HairSalonProject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairSalonProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairSalonProject2Application.class, args);
+	}
+
+}
