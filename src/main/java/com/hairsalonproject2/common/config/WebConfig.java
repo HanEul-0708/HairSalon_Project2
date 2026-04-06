65204128e0bb9d44580d9a8d21ee1b64a5934d95
이찬영 <dltkfksld1@naver.com>
2026-04-06 09:06:45 +0900

    member, reservation, board 브랜치 머지 후 수정

--- src/main/java/com/hairsalonproject2/common/config/WebConfig.java
+++ src/main/java/com/hairsalonproject2/common/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.hairsalonproject2.global.config;
+package com.hairsalonproject2.common.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
